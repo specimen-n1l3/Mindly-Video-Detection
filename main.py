5057a88e23ec126e49ac1f87c04fca947611692b
specimen-n1l3 <91599708+specimen-n1l3@users.noreply.github.com>
2023-07-16 12:34:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,101 @@
 import tensorflow as tf
-from tensorflow import keras
-from keras import layers
-
+from tensorflow.keras.utils import to_categorical, load_img
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+import pandas as pd
+import numpy as np
+import os
 import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+import random
+from tqdm.notebook import tqdm
+​
+warnings.filterwarnings('ignore')
 
-def model(shape, classes):
-    input = keras.Input(shape=shape)
+train_dir = '../input/facial-expression-dataset/train/train/'
+test_dir = '../input/facial-expression-dataset/test/test/'
 
-    a = layers.Rescaling(1.0 / 255)(input)
-    a = layers.Conv2D(128, 3, strides=2, padding='same')(a)
-    a = layers.BatchNormalization()(a)
-    a = layers.Activation("relu")(a)
+def load_dataset(directory):
+    img_paths = []
+    labels = []
+    
+    for label in os.listdir(directory):
+        for filename in os.listdir(directory+label):
+            img_path = os.path.join(directory, label, filename)
+            img_paths.append(img_path)
+            labels.append(label)
+            
+        print(label, "Completed")
+        
+    return img_paths, labels
 
-    block = a
+train = pd.DataFrame()
+train['image'], train['label'] = load_dataset(train_dir)
+train = train.sample(frac=1).reset_index(drop=True)
+train.head()
 
-    for x in [256, 512, 728]:
-        a = layers.Activation("relu")(a)
-        a = layers.SeparableConv2D(x, 3, padding='same')(a)
-        a = layers.BatchNormalization()(a)
-        a = layers.Activation("relu")(a)
-        a = layers.SeparableConv2D(x, 3, padding='same')(a)
-        a = layers.BatchNormalization()(a)
-        a = layers.MaxPooling2D(3, strides=2, padding='same')(a)
+test = pd.DataFrame()
+test['image'], test['label'] = load_dataset(test_dir)
+test = test.sample(frac=1).reset_index(drop=True)
+test.head()
 
-        r = layers.Conv2D(x, 1, strides=2, padding='same')(block)
+def extract_features(imgs):
+    features = []
+    for img in tqdm(imgs):
+        img = load_img(img, grayscale=True)
+        img = np.array(img)
+        features.append(img)
+    features = np.array(features)
+    features = features.reshape(len(features), 48, 48, 1)
+    return features
 
-        a = layers.add([a, r])
-        block = a
+train_features = extract_features(train['image'])
+test_features = extract_features(test['image'])
 
-    a = layers.SeparableConv2D(1024, 3, padding='same')(a)
-    a = layers.BatchNormalization()(a)
-    a = layers.Activation("relu")(a)
-    a = layers.GlobalAveragePooling2D()(a)
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+le.fit(train['label'])
+y_train = le.transform(train['label'])
+y_test = le.transform(test['label'])
 
-    #several emotions, so use softmax
-    if classes >= 2:
-        activation = 'softmax'
-        units = classes
+y_train = to_categorical(y_train, num_classes=7)
+y_test = to_categorical(y_test, num_classes=7)
+x_train = train_features/255.0
+x_test = test_features/255.0
+input_shape = (48, 48, 1)
+n_classes = 7
 
-    a = layers.Dropout(0.5)(a)
-    output = layers.Dense(units, activation=activation)(x)
-    return keras.Model(input, output)
+model = Sequential()
+model.add(Conv2D(128, kernel_size=(3,3), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.3))
+model.add(Conv2D(256, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.3))
+model.add(Conv2D(512, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.3))
+model.add(Conv2D(512, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.3))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(n_classes, activation='softmax'))
+model.compile(optimizer='adamax', loss='categorical_crossentropy', metrics='accuracy')
+
+trained_model = model.fit(x=x_train, y=y_train, batch_size=128, epochs=100, validation_data=(x_test, y_test))
+
+acc = trained_model.history['accuracy']
+val_acc = trained_model.history['val_accuracy']
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'b', label='Training Accuracy')
+plt.plot(epochs, val_acc, 'r', label='Validation Accuracy')
+plt.title('Accuracy Graph')
+plt.legend()
+plt.figure()
+plt.show()
